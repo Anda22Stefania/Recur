0e7c348eb5b2039ce98833a368fb4eca6e221297
George-Stefan Chiriluta <chirilutageorge@yahoo.com>
2018-10-28 00:09:54 +0100

    fixed minor issues

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import json, requests
 app = Flask(__name__)
 CORS(app)
 
-apiKey = "c4a0166d7cd592202bf2bd0bf909b21b"
+apiKey = "351388794358970b8ed7ec1790b2004a"
+
+@app.route('/')
+def index():
+    return "hi"
 
 @app.route('/api/accounts/<custId>')
 def userAccountsRoute(custId):
@@ -31,23 +35,25 @@ def userInfoRoute(custId):
             "code": 500
         })
 
-@app.route('/')
-def index():
-    return "hi"
-
 @app.route('/api/subscriptions/<id>', methods=['GET'])
 def getSubscriptionsForUser(id):
     subscriptions = []
-    url = "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(id, key)
-    content = json.loads(requests.get(url).text)
+    url = "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(id, apiKey)
+    response = requests.get(url)
+    if response.status_code != 200:
+        return jsonify({"status_code: 500"})
+    content = json.loads(response.text)
     subscriptions = [s for s in content if s['description'] == "recur"]
     return jsonify(subscriptions)
 
 @app.route('/api/irregular/<id>', methods=['GET'])
 def getIrregularRecurringPurchases(id):
     purchases = []
-    url = "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(id, key)
-    content = json.loads(requests.get(url).text)
+    url = "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(id, apiKey)
+    response = requests.get(url)
+    if response.status_code != 200:
+        return jsonify({"status_code: 500"})
+    content = json.loads(response.text)
     purchases = [p for p in content if p['description'] == "irregular"]
     return jsonify(purchases)
 
