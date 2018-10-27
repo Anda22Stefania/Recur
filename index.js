33305428eeb3f1bbcf4a16f6e11aac61cbdca43a
Ben <Ben@Piggott.eu>
2018-10-27 19:23:01 +0100

    Get Customer Accounts Function
    Retrieves a customer's accounts using customer id passed to the function. Currently just prints the account info into the console.

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ $.ajax({
     alert(result.data[1].name);
   }
 });
+
+function getCustomerAccounts(id) {
+    var url = "http://api.reimaginebanking.com/customers/" + {id} + "/accounts?key=c4a0166d7cd592202bf2bd0bf909b21b";
+    $.ajax(
+        {
+            type: "GET",
+            url: url,
+            success: function(results){
+                var customerAccounts = result;
+                console.log(customerAccounts);
+            }
+        }
+    )
+}
