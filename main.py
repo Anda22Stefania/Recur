094d05b44dbcf2e273991fb4fcd9fc53b6f5b5d0
Ben <Ben@Piggott.eu>
2018-10-28 09:59:28 +0000

    Middleware Version 1.0
    Middleware is now feature complete with all 4 endpoints functioning as intended.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 import json, requests
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
-apiKey = "351388794358970b8ed7ec1790b2004a"
+apiKey = "c4a0166d7cd592202bf2bd0bf909b21b"
 
 @app.route('/')
 def index():
@@ -32,43 +32,78 @@ def userInfoRoute(custId):
         return jsonify(data)
     else:
         return jsonify({
-            "code": 500
+            "status_code": 500
         })
 
 @app.route('/api/subscriptions/<id>', methods=['GET'])
 def getSubscriptionsForUser(id):
-    subscriptions = []
     url = "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(id, apiKey)
     response = requests.get(url)
     if response.status_code != 200:
         return jsonify({"status_code: 500"})
     content = json.loads(response.text)
-    subscriptions = [s for s in content if s['description'] == "recur"]
-    for s in subscriptions:
-        merchant = getMerchant(s['merchant_id'])
-        s.update({'merchant': merchant})
+    merchants = []
+    x = 0
+    while x < len(content):
+        merchants.append(content[x]["merchant_id"])
+        x += 1
+    categories = []
+    x = 0
+    while x < len(merchants):
+        url = "http://api.reimaginebanking.com/merchants/{}?key={}".format(merchants[x], apiKey)
+        if response.status_code != 200:
+            return jsonify({"status_code: 500"})
+        response = requests.get(url)
+        data = json.loads(response.text)
+        categories.append(data["category"])
+        x += 1
+    x = 0
+    subscriptions = []
+    while x < len(merchants):
+        if "utilities" in map(str.lower,categories[x]):
+            subscriptions.append(content[x])
+        elif "streaming" in map(str.lower,categories[x]):
+            subscriptions.append(content[x])
+        elif "transport" in map(str.lower,categories[x]):
+            subscriptions.append(content[x])
+        elif "gas" in map(str.lower,categories[x]):
+            subscriptions.append(content[x])
+        x += 1
     return jsonify(subscriptions)
 
 @app.route('/api/irregular/<id>', methods=['GET'])
 def getIrregularRecurringPurchases(id):
-    purchases = []
     url = "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(id, apiKey)
     response = requests.get(url)
     if response.status_code != 200:
         return jsonify({"status_code: 500"})
     content = json.loads(response.text)
-    purchases = [p for p in content if p['description'] == "irregular"]
-    for p in purchases:
-        merchant = getMerchant(p['merchant_id'])
-        p.update({'merchant': merchant})
+    merchants = []
+    x = 0
+    while x < len(content):
+        merchants.append(content[x]["merchant_id"])
+        x += 1
+    categories = []
+    x = 0
+    while x < len(merchants):
+        url = "http://api.reimaginebanking.com/merchants/{}?key={}".format(merchants[x], apiKey)
+        response = requests.get(url)
+        if response.status_code != 200:
+            return jsonify({"status_code: 500"})
+        data = json.loads(response.text)
+        categories.append(data["category"])
+        x += 1
+    x = 0
+    purchases = []
+    while x < len(merchants):
+        if "game" in map(str.lower, categories[x]):
+            purchases.append(content[x])
+        elif "food" in map(str.lower, categories[x]):
+            purchases.append(content[x])
+        elif "entertainment" in map(str.lower, categories[x]):
+            purchases.append(content[x])
+        x += 1
     return jsonify(purchases)
-
-def getMerchant(id):
-    url = "http://api.reimaginebanking.com/merchants/{}?key={}".format(id, apiKey)
-    response = requests.get(url)
-    if response.status_code != 200:
-        return jsonify({"status_code: 500"})
-    return json.loads(response.text)
 
 if __name__ == '__main__':
     app.run(debug=True)
