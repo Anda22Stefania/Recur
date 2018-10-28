04670179d5acf65c313b66322ebdb94e6bf50a8e
Ben <Ben@Piggott.eu>
2018-10-28 03:47:14 +0000

    Payment Creation Complete
    Program can now successfully create API calls to create purchases from a randomly determined list of merchants

--- testData/purchaseAlgorithm.py
+++ testData/purchaseAlgorithm.py
@@ -1,24 +1,36 @@
-import json, requests, random
+import json, requests, random, os, time
 
 # Hardcoded variables #
 
 consMerch = ["utilities","streaming","transport","gas"]
-vairMerch = ["food","entertainment","game"]
+variMerch = ["food","entertainment","game"]
 
 chosenConsMerch = []
 chosenVariMerch = []
 
 # Variable inputs #
 
-apiKey = input("Input API Key:\n")
+apiKey = "c4a0166d7cd592202bf2bd0bf909b21b" #input("Input API Key:\n")
+accountId = "5bd44926322fa06b67793e7c" #input("Input account id:\n")
 noConsMerch = int(input("Input number of desired constant merchants:\n"))
 noVariMerch = int(input("Input number of desired variable merchants:\n"))
 months = int(input("Number of months of purchase history:\n"))
 
-# Load JSON file of all merchants #
+## Functions ##
 
-json_data = open('merchants.json')
-data = json.load(json_data)
+# Function to fill merchants JSON file with merchants listed in API #
+
+def merchantsCall(apiKey):
+    url = 'http://api.reimaginebanking.com/merchants?key={}'.format(apiKey)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        with open('merchants.json', 'w') as outfile:
+            json.dump(data, outfile)
+        return "Success"
+    else:
+        return "Error"
+
 
 # Function to find list of all merchants that match a given category #
 
@@ -27,7 +39,7 @@ def merchList( category ):
     list = []
     while i < len(data):
         try:
-            dataCat = data[i]['category']
+            dataCat = data[i]["category"]
             try:
                 dataCat = "".join(dataCat)
             except:
@@ -35,7 +47,58 @@ def merchList( category ):
         except:
             name = ""
         if category in dataCat.lower():
-            list.append(data[i]['_id'])
+            list.append(data[i]["_id"])
         i += 1
     return list
+
+# Select merchants of given type #
+
+def chooseMerchants(type, number, list):
+    merchants = []
+
+    for x in type:
+        merchants += merchList(x)
+
+    for x in range(number):
+        list.append(merchants[random.randint(0,len(merchants)-1)])
+
+# Function to submit payments to API #
+
+def submitPayment(merchant, date):
+    url = 'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(accountId,apiKey)
+    payment = {
+        "merchant_id": merchant,
+        "medium": "balance",
+        "purchase_date": date,
+        "amount": random.randint(0,50)
+    }
+    response = requests.post(
+        url,
+        data=json.dumps(payment),
+        headers={'content-type': 'application/json'},
+    )
+    if response.status_code == 201:
+        print("good")
+        return True
+    else:
+        print("bad")
+        return False
+
+## Program ##
+
+# Update merchants JSON file if not updated in more than a week #
+
+if os.path.getmtime("merchants.json") < time.time() - 7 * 86400:
+    merchantsCall()
+
+# Load JSON file of all merchants #
+
+json_data = open("merchants.json")
+data = json.load(json_data)
+
+# Select merchants #
+
+chooseMerchants(consMerch, noConsMerch, chosenConsMerch)
+
+chooseMerchants(variMerch, noVariMerch, chosenVariMerch)
 
